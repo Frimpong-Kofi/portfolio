83bd7a223241363c76da441fa4af93062270a24d
frimpong-kofi <noeljoel61@gmail.com>
2022-05-23 21:28:21 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// INITIALIZING WOW
+const wow = new WOW();
+wow.init();
+
+var typed = new Typed(".type", {
+  strings: [
+    "Python Developer",
+    "Web Developer",
+    "Cyber Enthusiast",
+    "Freelancer",
+  ],
+  typeSpeed: 70,
+  backSpeed: 60,
+  loop: true,
+});
+
+// OWL CAROUSEL
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  speed: 900,
+  margin: 10,
+  items: 2,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+// PRELOADER FADEOUT
+$(window).on("load", function () {
+  $(".preloader").delay("1000").fadeOut("slow");
+});
